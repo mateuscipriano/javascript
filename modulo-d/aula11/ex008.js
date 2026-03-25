3ef4b5cc2b41ae1895f1d08777260a9c89979030
mateuscipriano <mateuscipriano2567@gmail.com>
2026-03-25 14:38:04 -0300

    aula 11

---
+++ modulo-d/aula11/ex008.js
@@ -0,0 +1,5 @@
+var n = 60
+if (n> 50) {
+    console.log(`mais q 50`)
+}
+console.log('se há algo acima, n > 50')
---
+++ modulo-d/aula11/ex009.js
@@ -0,0 +1,7 @@
+var país ='Brasil'
+if (país == 'Brasil') {
+    console.log('br')
+}
+else {
+    console.log('não é br')
+}
