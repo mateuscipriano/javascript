4cca9999db3e3786b1a10155b5998023dd93bf67
mateuscipriano <mateuscipriano2567@gmail.com>
2026-04-02 14:47:37 -0300

    Update script.js

--- modulo-e/aula14ex/ex018/script.js
+++ modulo-e/aula14ex/ex018/script.js
@@ -7,12 +7,22 @@ function tabuada(){
          var n = Number(num.value)
          var c = 1
          tab.innerHTML = ''
-         while (c <= 10){
+
+        for (var c ; c <= 10; c++){
+       var item = document.createElement('option')
+       item.text = `${n} x ${c} = ${n*c}`
+       item.value = `tab${c}`
+       tab.appendChild(item)
+      }
+    }
+
+     /* 
+     while (c <= 10){
             var item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
-         }
-    }
+      } 
+   */
 }
