b29538c5419818eea01c36dcb86f6bd096489821
mateuscipriano <mateuscipriano2567@gmail.com>
2026-05-04 11:32:21 -0300

    estudo

---
+++ estudos-diversos/funcoes/iife/ex01.js
@@ -0,0 +1,6 @@
+//IIFE (imediately Invoked Function Expression)
+
+let v = "variável"; //neste caso, ";" é necessário
+(function () {
+    console.log('função executada')
+}) ()  //"()" executa a função
